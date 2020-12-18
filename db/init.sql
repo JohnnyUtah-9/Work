e546075f797268e518a497212c0191f0ac9cc90a
JohnnyUtah-9 <sidney.b.johnson3@gmail.com>
2020-12-18 18:25:05 -0500

    Created project skeleton

--- db/init.sql
+++ db/init.sql
@@ -6,7 +6,11 @@ CREATE TABLE IF NOT EXISTS tblCitiesImport (
     `fldLat` NUMERIC(6, 4),
     `fldLong` NUMERIC(7, 4),
     `fldCountry` VARCHAR(19) CHARACTER SET utf8,
+<<<<<<< HEAD
     `fldAbbreviation` VARCHAR(3) CHARACTER SET utf8,
+=======
+    `fldAbreviation` VARCHAR(3) CHARACTER SET utf8,
+>>>>>>> c2ac05a (Created project skeleton)
     `fldCapitalStatus` VARCHAR(7) CHARACTER SET utf8,
     `fldPopulation` INT
 );
